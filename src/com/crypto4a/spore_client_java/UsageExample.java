774b663452f984847342ab0687c51c09ebfa7afb
avis <antoine.viscardi@gmail.com>
2018-08-07 14:33:28 -0400

    Added seeding local RNG example

--- src/com/crypto4a/spore_client_java/UsageExample.java
+++ src/com/crypto4a/spore_client_java/UsageExample.java
@@ -1,6 +1,9 @@
 package com.crypto4a.spore_client_java;
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Random;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,11 +22,20 @@ public class UsageExample {
 		String serverAddress = args[0];
 		SporeClient sporeClient = new SporeClient(serverAddress);
 		
+		System.out.println("Performing requests: ");
+		performRequests(sporeClient);
+		
+		System.out.println("Seeding local radnomness generator: ");
+		seedRNG(sporeClient);
+		
+	}
+	
+	public static void performRequests(SporeClient sporeClient) throws IOException, JSONException {
 		JSONObject response = sporeClient.doInfoRequest();
 		System.out.println("getInfo request:");
 		System.out.println("\tname: " + response.get("name"));
 		System.out.println("\tentropySize: " + response.get("entropySize"));
-//		System.out.println(response.toString(4));
+		System.out.println(response.toString(4));
 		System.out.println();
 		
 		response = sporeClient.doEntropyRequest("AwesomeChallenge");
@@ -41,6 +53,19 @@ public class UsageExample {
 		System.out.println("\tJWT: " + response.get("JWT"));
 		System.out.println(response.toString(4));
 		System.out.println();
+	}
+	
+	public static void seedRNG(SporeClient sporeClient) throws IOException, JSONException {
+		byte[] challenge = new byte[32];
+		new Random().nextBytes(challenge);
+		
+		String challengeB64 = Base64.getEncoder().encodeToString(challenge);
+		String entropy = sporeClient.doEntropyRequest(challengeB64).getString("entropy");
+		System.out.println("Received entropy: " + entropy);
+		
+		SecureRandom rng = new SecureRandom();
+		rng.setSeed(Base64.getDecoder().decode(entropy));
+		System.out.println("SecureRandom was seeded successfully.");
 	}
 
 }
